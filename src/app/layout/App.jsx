448ebea1bd33b54f8f2af8f0b6eadaa9361c58cd
Nick Kosik <Nicholas.kosik@gmail.com>
2021-09-02 20:39:58 -0500

    stopped at delete event

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -6,6 +6,7 @@ import EventDetailedPage from '../../features/events/eventDetailed/EventDetailed
 import EventForm from '../../features/events/eventForm/EventForm';
 import NavBar from '../../features/events/nav/NavBar';
 import HomePage from '../../features/home/HomePage';
+import Sandbox from '../../features/sandbox/Sandbox';
 import './styles.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <NavBar />
             <Container className='main'>
               <Route exact path='/events' component={EventDashboard} />
+              <Route exact path='/sandbox' component={Sandbox} />
               <Route path='/events/:id' component={EventDetailedPage} />
               <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
             </Container>
